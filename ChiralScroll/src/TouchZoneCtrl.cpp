e53345f884e7774b6e7ffac7be3f473eb42a9ac5
Jamison Wilde <jamison_wilde@yahoo.com>
2026-09-06 00:13:02 -0500

    feat: add Win32 TouchZoneCtrl custom control (unwired)

---
+++ ChiralScroll/src/TouchZoneCtrl.cpp
@@ -0,0 +1,242 @@
+#include "TouchZoneCtrl.h"
+
+#include <algorithm>
+#include <windowsx.h>
+
+namespace chiralscroll
+{
+
+namespace
+{
+
+bool NearPoint(POINT pt, POINT target, int slop)
+{
+	return pt.x >= target.x - slop && pt.x <= target.x + slop &&
+	       pt.y >= target.y - slop && pt.y <= target.y + slop;
+}
+
+}  // namespace
+
+void TouchZoneCtrl::RegisterWindowClass(HINSTANCE hInstance)
+{
+	WNDCLASSW wc{};
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = &TouchZoneCtrl::WndProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_BTNFACE + 1);
+	wc.lpszClassName = kTouchZoneCtrlClass;
+	RegisterClassW(&wc);
+}
+
+TouchZoneCtrl* TouchZoneCtrl::FromHandle(HWND hwnd)
+{
+	return reinterpret_cast<TouchZoneCtrl*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+}
+
+void TouchZoneCtrl::SetZones(float vZone, float hZone)
+{
+	vZone_ = std::clamp(vZone, 0.0f, 1.0f);
+	hZone_ = std::clamp(hZone, 0.0f, 1.0f);
+	InvalidateRect(hwnd_, nullptr, TRUE);
+}
+
+LRESULT CALLBACK TouchZoneCtrl::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	if(msg == WM_NCCREATE)
+	{
+		SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(new TouchZoneCtrl(hwnd)));
+		return TRUE;
+	}
+	TouchZoneCtrl* ctrl = FromHandle(hwnd);
+	if(!ctrl)
+	{
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	if(msg == WM_NCDESTROY)
+	{
+		SetWindowLongPtr(hwnd, GWLP_USERDATA, 0);
+		delete ctrl;
+		return 0;
+	}
+	return ctrl->HandleMessage(msg, wParam, lParam);
+}
+
+LRESULT TouchZoneCtrl::HandleMessage(UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch(msg)
+	{
+		case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC dc = BeginPaint(hwnd_, &ps);
+			RECT client;
+			GetClientRect(hwnd_, &client);
+			Paint(dc, client);
+			EndPaint(hwnd_, &ps);
+			return 0;
+		}
+		case WM_LBUTTONDOWN:
+		{
+			RECT client;
+			GetClientRect(hwnd_, &client);
+			const POINT pt{GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam)};
+			if(NearPoint(pt, VGrabberPos(client), kGrabberSize/2 + kHitSlop))
+			{
+				drag_ = Drag::kVertical;
+			}
+			else if(NearPoint(pt, HGrabberPos(client), kGrabberSize/2 + kHitSlop))
+			{
+				drag_ = Drag::kHorizontal;
+			}
+			if(drag_ != Drag::kNone)
+			{
+				SetCapture(hwnd_);
+			}
+			return 0;
+		}
+		case WM_MOUSEMOVE:
+		{
+			if(drag_ == Drag::kNone)
+			{
+				return 0;
+			}
+			RECT client;
+			GetClientRect(hwnd_, &client);
+			const int width = client.right - client.left;
+			const int height = client.bottom - client.top;
+			if(drag_ == Drag::kVertical && width > 0)
+			{
+				vZone_ = std::clamp(1.0f - static_cast<float>(GET_X_LPARAM(lParam))/width, 0.0f, 1.0f);
+			}
+			else if(drag_ == Drag::kHorizontal && height > 0)
+			{
+				hZone_ = std::clamp(1.0f - static_cast<float>(GET_Y_LPARAM(lParam))/height, 0.0f, 1.0f);
+			}
+			InvalidateRect(hwnd_, nullptr, TRUE);
+			NotifyParent();
+			return 0;
+		}
+		case WM_LBUTTONUP:
+			if(drag_ != Drag::kNone)
+			{
+				ReleaseCapture();
+			}
+			drag_ = Drag::kNone;
+			return 0;
+		case WM_CAPTURECHANGED:
+			drag_ = Drag::kNone;
+			return 0;
+		case WM_SETCURSOR:
+		{
+			POINT pt;
+			GetCursorPos(&pt);
+			ScreenToClient(hwnd_, &pt);
+			RECT client;
+			GetClientRect(hwnd_, &client);
+			if(drag_ == Drag::kVertical || NearPoint(pt, VGrabberPos(client), kGrabberSize/2 + kHitSlop))
+			{
+				SetCursor(LoadCursor(nullptr, IDC_SIZEWE));
+			}
+			else if(drag_ == Drag::kHorizontal || NearPoint(pt, HGrabberPos(client), kGrabberSize/2 + kHitSlop))
+			{
+				SetCursor(LoadCursor(nullptr, IDC_SIZENS));
+			}
+			else
+			{
+				SetCursor(LoadCursor(nullptr, IDC_ARROW));
+			}
+			return TRUE;
+		}
+		case WM_ENABLE:
+			InvalidateRect(hwnd_, nullptr, TRUE);
+			return 0;
+	}
+	return DefWindowProc(hwnd_, msg, wParam, lParam);
+}
+
+void TouchZoneCtrl::Paint(HDC dc, const RECT& client) const
+{
+	const int width = client.right - client.left;
+	const int height = client.bottom - client.top;
+
+	HRGN clipRgn = CreateRoundRectRgn(0, 0, width + 1, height + 1, kCornerSize, kCornerSize);
+	SelectClipRgn(dc, clipRgn);
+
+	HBRUSH padBrush = CreateSolidBrush(RGB(128, 128, 128));
+	FillRect(dc, &client, padBrush);
+
+	if(IsWindowEnabled(hwnd_))
+	{
+		const int vEdge = static_cast<int>(width*(1.0f - vZone_));
+		const int hEdge = static_cast<int>(height*(1.0f - hZone_));
+		const int oldBkMode = SetBkMode(dc, TRANSPARENT);
+
+		if(hZone_ > 0)
+		{
+			HBRUSH hatch = CreateHatchBrush(HS_BDIAGONAL, RGB(200, 0, 0));
+			RECT zone{0, hEdge, vEdge, height};
+			FillRect(dc, &zone, hatch);
+			DeleteObject(hatch);
+			HPEN pen = CreatePen(PS_SOLID, 1, RGB(200, 0, 0));
+			HGDIOBJ oldPen = SelectObject(dc, pen);
+			MoveToEx(dc, 0, hEdge, nullptr);
+			LineTo(dc, vEdge, hEdge);
+			SelectObject(dc, oldPen);
+			DeleteObject(pen);
+		}
+		if(vZone_ > 0)
+		{
+			HBRUSH hatch = CreateHatchBrush(HS_FDIAGONAL, RGB(0, 160, 0));
+			RECT zone{vEdge, 0, width, height};
+			FillRect(dc, &zone, hatch);
+			DeleteObject(hatch);
+			HPEN pen = CreatePen(PS_SOLID, 1, RGB(0, 160, 0));
+			HGDIOBJ oldPen = SelectObject(dc, pen);
+			MoveToEx(dc, vEdge, 0, nullptr);
+			LineTo(dc, vEdge, height);
+			SelectObject(dc, oldPen);
+			DeleteObject(pen);
+		}
+		SetBkMode(dc, oldBkMode);
+
+		HBRUSH black = static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH));
+		const POINT v = VGrabberPos(client);
+		const POINT h = HGrabberPos(client);
+		RECT vg{v.x - kGrabberSize/2, v.y - kGrabberSize/2, v.x + kGrabberSize/2 + 1, v.y + kGrabberSize/2 + 1};
+		RECT hg{h.x - kGrabberSize/2, h.y - kGrabberSize/2, h.x + kGrabberSize/2 + 1, h.y + kGrabberSize/2 + 1};
+		FillRect(dc, &vg, black);
+		FillRect(dc, &hg, black);
+	}
+
+	DeleteObject(padBrush);
+	SelectClipRgn(dc, nullptr);
+	DeleteObject(clipRgn);
+}
+
+POINT TouchZoneCtrl::VGrabberPos(const RECT& client) const
+{
+	const int width = client.right - client.left;
+	const int height = client.bottom - client.top;
+	return {static_cast<LONG>(width*(1.0f - vZone_)), height/2};
+}
+
+POINT TouchZoneCtrl::HGrabberPos(const RECT& client) const
+{
+	const int width = client.right - client.left;
+	const int height = client.bottom - client.top;
+	return {width/2, static_cast<LONG>(height*(1.0f - hZone_))};
+}
+
+void TouchZoneCtrl::NotifyParent() const
+{
+	HWND parent = GetParent(hwnd_);
+	if(parent)
+	{
+		SendMessage(parent, WM_COMMAND,
+			MAKEWPARAM(GetDlgCtrlID(hwnd_), TZCN_CHANGED),
+			reinterpret_cast<LPARAM>(hwnd_));
+	}
+}
+
+}  // namespace chiralscroll
---
+++ ChiralScroll/src/TouchZoneCtrl.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <Windows.h>
+
+namespace chiralscroll
+{
+
+// Window class name, usable in dialog templates. RegisterWindowClass must be
+// called before any window using it is created.
+inline constexpr wchar_t kTouchZoneCtrlClass[] = L"ChiralTouchZoneCtrl";
+
+// WM_COMMAND notification code sent to the parent when a grabber drag changes
+// a zone value.
+inline constexpr WORD TZCN_CHANGED = 1;
+
+// Draws the touchpad as a grey rounded rectangle with the vertical scroll
+// zone (green hatch, from the right edge) and horizontal scroll zone (red
+// hatch, from the bottom edge) plus two draggable grabber squares. Zones are
+// fractions in [0, 1].
+class TouchZoneCtrl
+{
+public:
+	static void RegisterWindowClass(HINSTANCE hInstance);
+	static TouchZoneCtrl* FromHandle(HWND hwnd);
+
+	void SetZones(float vZone, float hZone);
+	float vZone() const { return vZone_; }
+	float hZone() const { return hZone_; }
+
+private:
+	explicit TouchZoneCtrl(HWND hwnd) : hwnd_(hwnd) {}
+
+	static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	LRESULT HandleMessage(UINT msg, WPARAM wParam, LPARAM lParam);
+
+	void Paint(HDC dc, const RECT& client) const;
+	POINT VGrabberPos(const RECT& client) const;
+	POINT HGrabberPos(const RECT& client) const;
+	void NotifyParent() const;
+
+	enum class Drag { kNone, kVertical, kHorizontal };
+
+	// Grabber square size matches the old wx control.
+	static constexpr int kGrabberSize = 7;
+	static constexpr int kHitSlop = 5;
+	static constexpr int kCornerSize = 13;
+
+	HWND hwnd_;
+	float vZone_ = 0.0f;
+	float hZone_ = 0.0f;
+	Drag drag_ = Drag::kNone;
+};
+
+}  // namespace chiralscroll
